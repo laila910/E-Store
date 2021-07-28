b63ff345cd081199bc29d9ec186aa1ade7a7b0b2
Laila Ibrahim <laila457@h-eng.helwan.edu.eg>
2021-07-28 22:34:45 +0200

    commit on master

---
+++ Dashboard/admin/orderdetails/index.php
@@ -0,0 +1,110 @@
+<?php
+ include '../helpers/functions.php';
+  include '../helpers/dbconnection.php';
+
+
+  $sql="SELECT *, `orders`.`id` as `order_id` FROM `orders` join `orderdetailes` on `orderdetailes`.`ordernumber` = `orders`.`id`";
+ // here ,remeber to change quantity from here to orders 
+  $op  = mysqli_query($conn,$sql); 
+
+
+include '../header.php';
+?>
+
+<body class="sb-nav-fixed">
+   <?php
+
+include '../nav.php';
+?>
+    <div id="layoutSidenav">
+<?php
+
+include '../sidNave.php';
+?>
+        <div id="layoutSidenav_content">
+            <main>
+               <div class="container-fluid">
+                        <h1 class="mt-4">Tables</h1>
+                        <ol class="breadcrumb mb-4">
+
+
+                        <?php 
+                        
+
+                        if(isset($_SESSION['errors'])){
+
+                           foreach($_SESSION['errors'] as $key =>  $value){
+
+                            echo '* '.$key.' : '.$value.'<br>';
+                           }
+
+                             unset($_SESSION['errors']);
+                         }else{
+                    ?>
+                    
+                            <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+                            <li class="breadcrumb-item active">Product Data</li>
+                    <?php } ?>
+               
+                        
+                        
+                        </ol>
+    
+                        <div class="card mb-4">
+                            <div class="card-header">
+                                <i class="fas fa-table mr-1"></i>
+                                Product Table
+                            </div>
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                        <thead>
+                                            <tr>
+                                                <th>#</th>
+                                                <th>Order Number  </th>
+                                                <th>shippingcost </th>
+                                                <th>quntity</th>
+                                                <th>discount</th>
+                                                <th>totalprice </th>
+                                              
+                                                <th>Action</th>
+                                          
+                                            </tr>
+                                        </thead>
+                                  
+                                    
+                                        <tbody>
+                                       
+                             <?php 
+                           
+                                while($result = mysqli_fetch_assoc($op)){
+                                   
+                             
+                             ?>           
+                                        <tr>
+                                                <td><?php echo $result['id'];?></td>
+                                                <td><?php echo $result['ordernumber'];?></td>
+                                                <td><?php echo $result['shippingcost'];?></td>
+                                                <td><?php echo $result['quntity'];?></td>
+                                                <td><?php echo $result['discount'];?></td>
+                                                <td><?php echo $result['totalprice'];?></td>
+                                              
+                                                <td>
+
+                                                <a href='delete.php?id=<?php echo $result['id'];?>' class='btn btn-danger m-r-1em'>Delete</a>
+                                                <a href='edit.php?id=<?php echo $result['id'];?>' class='btn btn-primary m-r-1em'>Edit</a> 
+                                               
+                                                </td>
+                                  
+                                        </tr>
+                            <?php } ?>             
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            </main>
+ <?php
+include '../footer.php';
+ ?>
