115f0c0914941eefe93d064e6f4fb44507dbd583
Laila Ibrahim <laila457@h-eng.helwan.edu.eg>
2021-08-05 15:19:03 +0200

    commit on master

--- Dashboard/login.php
+++ Dashboard/login.php
@@ -89,75 +89,76 @@ include 'navbar.php'; ?>
 
 
         ?>
-
-        <div class="row">
-
-
-            <div class="col-lg-6">
+        <?php if (!isset($_SESSION['users']['id'])) { ?>
+            <div class="row">
 
 
-                <form action="includes/signup.inc.php" method="post">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <label>First Name</label>
-                            <input class="form-control" type="text" placeholder="First Name" name="firstName">
-                        </div>
-                        <div class="col-md-6">
-                            <label>Last Name"</label>
-                            <input class="form-control" type="text" placeholder="Last Name" name="lastName">
-                        </div>
-                        <div class="col-md-6">
-                            <label>E-mail</label>
-                            <input class="form-control" type="text" placeholder="E-mail" name="email">
-                        </div>
-                        <div class="col-md-6">
-                            <label>Mobile No</label>
-                            <input class="form-control" type="text" placeholder="Mobile No" name="mobileNo">
-                        </div>
-                        <div class="col-md-6">
-                            <label>Password</label>
-                            <input class="form-control" type="text" placeholder="Password" name="password">
-                        </div>
-                        <div class="col-md-6">
-                            <label>Retype Password</label>
-                            <input class="form-control" type="text" placeholder="Password" name="Repeat-password">
-                        </div>
-                        <div class="col-md-12">
-                            <button class="btn">Sign-Up</button>
-                        </div>
-                    </div>
-                </form>
-            </div>
-            <div class="col-lg-6">
-                <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <label>E-mail </label>
-                            <input class="form-control" type="text" placeholder="E-mail / Username" name="email">
-                        </div>
-                        <div class="col-md-6">
-                            <label>Password</label>
-                            <input class="form-control" type="text" placeholder="Password" name="password">
-                        </div>
-                        <div class="col-md-12">
-                            <div class="custom-control custom-checkbox">
-                                <input type="checkbox" class="custom-control-input" id="newaccount">
-                                <label class="custom-control-label" for="newaccount">Keep me signed in</label>
+                <div class="col-lg-6">
+
+
+                    <form action="includes/signup.inc.php" method="post">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <label>First Name</label>
+                                <input class="form-control" type="text" placeholder="First Name" name="firstName">
+                            </div>
+                            <div class="col-md-6">
+                                <label>Last Name"</label>
+                                <input class="form-control" type="text" placeholder="Last Name" name="lastName">
+                            </div>
+                            <div class="col-md-6">
+                                <label>E-mail</label>
+                                <input class="form-control" type="text" placeholder="E-mail" name="email">
+                            </div>
+                            <div class="col-md-6">
+                                <label>Mobile No</label>
+                                <input class="form-control" type="text" placeholder="Mobile No" name="mobileNo">
+                            </div>
+                            <div class="col-md-6">
+                                <label>Password</label>
+                                <input class="form-control" type="text" placeholder="Password" name="password">
+                            </div>
+                            <div class="col-md-6">
+                                <label>Retype Password</label>
+                                <input class="form-control" type="text" placeholder="Password" name="Repeat-password">
+                            </div>
+                            <div class="col-md-12">
+                                <button class="btn">Sign-Up</button>
                             </div>
                         </div>
-                        <div class="col-md-12">
-                            <button class="btn">Login</button>
+                    </form>
+                </div>
+                <div class="col-lg-6">
+                    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <label>E-mail </label>
+                                <input class="form-control" type="text" placeholder="E-mail / Username" name="email">
+                            </div>
+                            <div class="col-md-6">
+                                <label>Password</label>
+                                <input class="form-control" type="text" placeholder="Password" name="password">
+                            </div>
+                            <div class="col-md-12">
+                                <div class="custom-control custom-checkbox">
+                                    <input type="checkbox" class="custom-control-input" id="newaccount">
+                                    <label class="custom-control-label" for="newaccount">Keep me signed in</label>
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                <button class="btn">Login</button>
+                            </div>
                         </div>
-                    </div>
-                </form>
+                    </form>
+                </div>
+
+
+
             </div>
-
-
-
-        </div>
 
 
     </div>
+<?php } ?>
 </div>
 <!-- Login End -->
 <?php include 'footer.php';
